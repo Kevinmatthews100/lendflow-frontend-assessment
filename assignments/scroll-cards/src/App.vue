b88d20f4b9ed2a54c1c7c5e284d1da05b33dc4e8
Kevin Matthews <kevinmatthews100@gmail.com>
2024-10-12 10:35:03 -0400

    add scroll cards assignment

---
+++ assignments/scroll-cards/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Card from './Card.vue'
+
+const cards = Array.from(Array(100), (_,x) => `Card ${x+1}`);
+
+// Add code to this file to complete the task
+</script>
+
+<template>
+    <div>
+      <Card v-for="(card, id) in cards" :cardText="card" :key="id">
+        <p>{{card}}</p>
+      </Card>
+    </div>
+</template>
+
+<style scoped>
+</style>
